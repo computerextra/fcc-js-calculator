48cc49a18084210aa38c6488132b1c5ca360746e
computerextra <81916339+computerextra@users.noreply.github.com>
2023-07-04 17:56:15 +0200

    Init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Container } from "react-bootstrap";
+
+function App() {
+  return (
+    <Container className="mt-5">
+      <h1 className="text-center">FCC: Javascript Calculator</h1>
+    </Container>
+  );
+}
+
+export default App;
